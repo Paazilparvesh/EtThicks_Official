fcd18975bd19a0c20da81abba2c2b499a3412d0f
dhilip143 <dhilip146@gmail.com>
2025-09-18 15:59:18 +0530

    dhilip

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import Brand from "../component/home/brand";
 import Core from "../component/home/core";
-import Etthics from "../component/home/Ethiticsfull";
+
 import Faq from "../component/home/faq";
 import Get from "../component/home/get";
 import Second from "../component/home/second";
@@ -10,7 +10,7 @@ function Home() {
     return(
 <>
 <Second/>
-<Etthics/>
+
 <Get/>
 <Faq/>
 <Whyethitics/>
