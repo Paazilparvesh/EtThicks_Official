6e8c3c2748d9c077b6a7e858d63bb3c04c8b0e16
jeroleon <jebamalaijero.24it@licet.ac.in>
2025-09-19 00:30:51 +0530

    jero hero2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,14 +4,18 @@ import Core from "../component/home/core";
 import Faq from "../component/home/faq";
 import Get from "../component/home/get";
 import Hero from "../component/home/Hero";
+import Hero2 from "../component/home/Hero2";
 import Second from "../component/home/second";
 import Serve from "../component/home/Serve";
+
 import Whyethitics from "../component/home/whyethitics";
 
 function Home() {
     return(
 <>
-<Hero/>
+<Hero2/>
+
+
 <Serve/>
 <Second/>
 
