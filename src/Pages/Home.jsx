32d2228cf3dc328de69cce73dbb8d6af2b63b04a
jeroleon <jebamalaijero.24it@licet.ac.in>
2025-09-19 12:05:34 +0530

    jero mission

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,16 +5,19 @@ import Faq from "../component/home/faq";
 import Get from "../component/home/get";
 import Hero from "../component/home/Hero";
 import Hero2 from "../component/home/Hero2";
+import Mission from "../component/home/Mission";
 import Second from "../component/home/second";
 import Serve from "../component/home/Serve";
 import Team from "../component/home/Team";
 
 import Whyethitics from "../component/home/whyethitics";
 
+
 function Home() {
   return (
     <>
-      <Hero2 />
+      <Mission/>  
+      {/* <Hero2 />
 
       <Serve />
       <Team />
@@ -24,7 +27,7 @@ function Home() {
       <Faq />
       <Whyethitics />
       <Brand />
-      <Core />
+      <Core /> */}
     </>
   );
 }
