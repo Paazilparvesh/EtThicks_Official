b892acc8254b7cb183c83688f1f23d779ad412dc
dhilip143 <kdhilip146@gmail.com>
2025-09-19 13:04:55 +0530

    upde

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,34 +1,39 @@
 import Blog from "../component/home/blog";
-import Brand from "../component/home/brand";
-import Core from "../component/home/core";
-
+// import Brand from "../component/home/brand";
+// import Core from "../component/home/core";
 import Faq from "../component/home/faq";
 import Get from "../component/home/get";
-import Hero from "../component/home/Hero";
-import Second from "../component/home/second";
-import Serve from "../component/home/Serve";
-import Team from "../component/home/Team";
-
+import Hero2 from "../component/home/Hero2";
 import Whyethitics from "../component/home/whyethitics";
+import Person from "../component/home/Person";
+import Serve from "../component/home/Serve"; 
+import Team from "/src/component/home/Team.jsx"
+import Mission from "/src/component/home/Mission.jsx"  
+import Second from "../component/home/second";
 
 function Home() {
   return (
     <>
       <Hero2 />
-
-      <Serve />
-      <Mission />
-      <Team />
-      <Second />
-
-<Get/>
-<Faq/>
-<Whyethitics/>
-<Brand/>
-<Core/>
-<Blog/>
-<Person/>
-</>
-   );
+      <Second/>
+       <Serve /> 
+        <Mission/>  
+          <Team/> 
+          <Person />
+      <Whyethitics />
+        <Blog />
+      <Faq />
+      
+      {/* <Brand />
+      <Core /> */}
+     
+      
+    
+      
+    
+      <Get />
+    </>
+  );
 }
+
 export default Home;
