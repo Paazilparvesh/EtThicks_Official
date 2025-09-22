109faa551cb642ace3aba713667176153054a40a
dhilip143 <kdhilip146@gmail.com>
2025-09-22 16:44:47 +0530

    caps

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,39 +1,32 @@
-import Blog from "../component/home/blog";
+import Blog from "../component/home/Blog";
 // import Brand from "../component/home/brand";
 // import Core from "../component/home/core";
-import Faq from "../component/home/faq";
-import Get from "../component/home/get";
+import Faq from "../component/home/Faq";
+import Get from "../component/home/Get";
 import Hero2 from "../component/home/Hero2";
-import Whyethitics from "../component/home/whyethitics";
+import Whyethitics from "../component/home/Whyethitics";
 import Person from "../component/home/Person";
-import Serve from "../component/home/Serve"; 
+import Serve from "../component/home/Serve";
 import Team from "/src/component/home/Team.jsx"
-import Mission from "/src/component/home/Mission.jsx"  
-import Second from "../component/home/second";
+import Mission from "/src/component/home/Mission.jsx"
+import Second from "../component/home/Second";
 
 
 function Home() {
   return (
     <>
-     
+
       <Hero2 />
-      <Second/>
-       <Serve /> 
-        <Mission/>  
-          <Team/> 
-          <Person />
+      <Second />
+      <Serve />
+      <Mission />
+      <Team />
+      <Person />
       <Whyethitics />
-        <Blog />
-        
+      <Blog />
       <Faq />
-      
       {/* <Brand />
       <Core /> */}
-     
-      
-    
-      
-    
       <Get />
     </>
   );
