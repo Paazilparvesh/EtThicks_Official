b86437308115f96daad568648d0c4757d80085a0
dhilip143 <kdhilip146@gmail.com>
2025-11-20 11:26:25 +0530

    fir

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -3,7 +3,9 @@ import Count from '../Components/Aboutcomponents/count'
 import Pockyman from '../Components/Aboutcomponents/pockyman'
 import Create from '../Components/Aboutcomponents/create'
 import Client from '../Components/Aboutcomponents/client'
-
+import TeamSection from "/src/Components/HomeComponents/TeamSection.jsx"
+import QuoteSection from "/src/Components/HomeComponents/QuoteSection.jsx";
+import CTASection from "/src/Components/HomeComponents/CTASection.jsx";
 const AboutPage = () => {
   return (
     <>
@@ -11,6 +13,9 @@ const AboutPage = () => {
     <Pockyman/>
     <Create/>
     <Client/>
+    <TeamSection />
+    <QuoteSection />
+     <CTASection />
     </>
   )
 }
